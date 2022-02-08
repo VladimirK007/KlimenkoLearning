0430b94e83d6b6f5cf1273d8ef671bfc2aa1511e
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-08 17:40:04 +0200

    3task added

---
+++ src/HW1_DataTypes/ConvertCtoK3.java
@@ -0,0 +1,10 @@
+package HW1_DataTypes;
+
+public class ConvertCtoK3 {
+    public static void main(String[] args) {
+        double celciusTemperature = 30;
+        double kelvinTemparature = celciusTemperature + 273.15;
+        System.out.println("For celcius temperature " + celciusTemperature
+                + " Kelvin temperature is: " + kelvinTemparature);
+    }
+}
