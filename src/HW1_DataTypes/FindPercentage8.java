4672e6bbdc0d1e41958031a2d7e6ae2d4c7c53ca
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-08 18:42:22 +0200

    8task added

---
+++ src/HW1_DataTypes/FindPercentage8.java
@@ -0,0 +1,14 @@
+package HW1_DataTypes;
+
+public class FindPercentage8 {
+    public static void main(String[] args) {
+        double total_test = 10;
+        double marked_test = 8;
+        double result = (marked_test / total_test) * 100;
+        System.out.println("Results: " + result + "%");
+
+
+
+    }
+
+}
