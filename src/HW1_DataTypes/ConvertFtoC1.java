0aa008f6797899a61b807ad8948a2aca6c22ebd2
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-08 17:25:59 +0200

    Initial commit

---
+++ src/HW1_DataTypes/ConvertFtoC1.java
@@ -0,0 +1,16 @@
+package HW1_DataTypes;
+
+public class ConvertFtoC1 {
+
+    public static void main(String[] args) {
+
+        double fahrenheitTemperature = 77;
+        double celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+
+        System.out.println("For fahrenheit temperature " + fahrenheitTemperature
+                + " celsius temperature is: " + celsiusTemperature);
+
+    }
+
+
+}
