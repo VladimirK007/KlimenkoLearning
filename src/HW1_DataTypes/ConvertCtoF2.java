ba4e28d97874bc5bb51644fb029d4882cc1e9f57
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-08 17:35:12 +0200

    2task added

---
+++ src/HW1_DataTypes/ConvertCtoF2.java
@@ -0,0 +1,13 @@
+package HW1_DataTypes;
+
+public class ConvertCtoF2 {
+    public static void main(String[] args) {
+        double celciusTemperature = 30;
+        double fahrenheitTemparature = ((celciusTemperature * 9 / 5) + 32);
+        System.out.println("For celcius temperature " + celciusTemperature
+                + " fahrenheit temperature is " + fahrenheitTemparature);
+
+
+    }
+
+}
--- src/HW1_DataTypes/ConvertFtoC1.java
+++ src/HW1_DataTypes/ConvertFtoC1.java
@@ -5,7 +5,7 @@ public class ConvertFtoC1 {
     public static void main(String[] args) {
 
         double fahrenheitTemperature = 77;
-        double celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+        double celsiusTemperature = (fahrenheitTemperature - 32) * 5 / 9;
 
         System.out.println("For fahrenheit temperature " + fahrenheitTemperature
                 + " celsius temperature is: " + celsiusTemperature);
