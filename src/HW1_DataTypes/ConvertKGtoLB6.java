54bfe8df7e81a304682c96f69dbe847deb5f0e16
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-08 18:03:47 +0200

    6task added

---
+++ src/HW1_DataTypes/ConvertKGtoLB6.java
@@ -0,0 +1,17 @@
+package HW1_DataTypes;
+
+public class ConvertKGtoLB6 {
+    public static void main(String[] args) {
+        double kg1 = 10;
+        double lb1 = kg1 * 2.2046;
+        System.out.println("For Kilograms = " + kg1 + " Pounds is: " + lb1);
+
+        // revers lb to kg
+
+        double lb2 = 22.046;
+        double kg2 = lb2 / 2.2046;
+        System.out.println("For Pounds = " + lb2 + " Kilograms is: " + kg2);
+
+
+    }
+}
