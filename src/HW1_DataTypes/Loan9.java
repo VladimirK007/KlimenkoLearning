a918900f359fe29bb843d654e93aa8bc8a62906f
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-10 17:01:25 +0200

    9task added

---
+++ src/HW1_DataTypes/Loan9.java
@@ -0,0 +1,17 @@
+package HW1_DataTypes;
+
+public class Loan9 {
+    public static void main(String[] args) {
+
+
+    int husband = 34;
+    int wife = 34;
+
+    boolean loan = (husband | wife) <= 35;
+           System.out.println(loan);
+
+    }
+
+    }
+
+
