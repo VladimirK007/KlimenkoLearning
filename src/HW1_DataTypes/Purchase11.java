9f08d27ec22ea708721accba8fed08d56238350a
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-10 17:34:53 +0200

    11task added

---
+++ src/HW1_DataTypes/Purchase11.java
@@ -0,0 +1,13 @@
+package HW1_DataTypes;
+
+public class Purchase11 {
+    public static void main(String[] args) {
+        boolean variety1 = false;
+        boolean variety2 = true;
+        boolean availability = variety1 | variety2;
+        System.out.println(availability);
+
+
+    }
+
+}
