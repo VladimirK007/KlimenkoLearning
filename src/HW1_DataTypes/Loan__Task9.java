54499373ea37e7e0c8f41b920d735108f2eddac4
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-15 18:01:04 +0200

    method boolean added

--- src/HW1_DataTypes/Loan__Task9.java
+++ src/HW1_DataTypes/Loan__Task9.java
@@ -1,17 +1,19 @@
 package HW1_DataTypes;
 
 public class Loan__Task9 {
-    public static void main(String[] args) {
 
-
-        int husband = 35;
-        int wife = 35;
-
-        boolean loan = (husband <=35) && (wife <= 35);
-        System.out.println(loan);
+    public boolean loan(int husbandAge, int wifeAge){
+        boolean loanCheck = (husbandAge <=35) && (wifeAge<=35);
+        return loanCheck;
 
     }
-
 }
 
+class Parameters{
+    public static void main(String[] args){
+        Loan__Task9 l=new Loan__Task9();
+        boolean result = l.loan(34,36);
+        System.out.println(result);
 
+    }
+}
