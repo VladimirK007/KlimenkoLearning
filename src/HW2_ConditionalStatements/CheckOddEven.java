9c17373df67d79cc979ca57c3aed07b53d65f519
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-15 20:28:14 +0200

    2 tasks

---
+++ src/HW2_ConditionalStatements/CheckOddEven.java
@@ -0,0 +1,15 @@
+package HW2_ConditionalStatements;
+
+public class CheckOddEven {
+    public static void main(String[] args) {
+
+        int number = 11;
+
+        if (number %2!= 0) {
+            System.out.println("Odd number");
+        } else {
+            System.out.println("Even Number");
+        }
+
+    }
+}
