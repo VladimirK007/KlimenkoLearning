d79b6becbb2a3190ef94137fd2e3a60a56d1bd35
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-21 22:54:14 +0200

    15 task added

--- src/HW2_ConditionalStatements/MaxMidMin.java
+++ src/HW2_ConditionalStatements/MaxMidMin.java
@@ -4,47 +4,48 @@ import java.util.Scanner;
 
 public class MaxMidMin {
 
-    static int aVar(int a, int b, int c) {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.print("Enter tha value for a: ");
+        int a = s.nextInt();
+
+        System.out.print("Enter tha value for b: ");
+        int b = s.nextInt();
+
+        System.out.print("Enter tha value for c: ");
+        int c = s.nextInt();
 
         if ((a < b) && (a < c)) {
-            System.out.println("a is min");
-        } else if ((a > b) && (a < c) || ((a < b) && (a > c))) {
-            System.out.println("a is mid");
-        } else if ((a > b) && (a > c)) {
-            System.out.println("a is max");
+            System.out.println("The value a = " + a + " is min");
         }
-        return a;
-    }
+        if ((a > b) && (a < c) || ((a < b) && (a > c))) {
+            System.out.println("The value a = " + a + " is mid");
+        }
+        if ((a > b) && (a > c)) {
+            System.out.println("The value a = " + a + " is max");
+        }
 
-    static int bVar(int a, int b, int c) {
         if ((b < a) && (b < c)) {
-            System.out.println("b is min");
-        } else if ((b > a) && (b < c) || ((b < a) && (b > c))) {
-            System.out.println("b is mid");
-        } else if ((b > a) && (b > c)) {
-            System.out.println("b is max");
+            System.out.println("The value b = " + b + " is min");
         }
-        return b;
-    }
+        if ((b > a) && (b < c) || ((b < a) && (b > c))) {
+            System.out.println("The value b = " + b + " is mid");
+        }
+        if ((b > a) && (b > c)) {
+            System.out.println("The value b = " + b + " is max");
+        }
 
-    static int cVar(int a, int b, int c) {
         if ((c < a) && (c < b)) {
-            System.out.println("c is min");
-        } else if ((c > a) && (c < b) || ((c < a) && (c > b))) {
-            System.out.println("c is mid");
-        } else if ((c > a) && (c > b)) {
-            System.out.println("c is max");
+            System.out.println("The value c = " + c + " is min");
         }
-        return c;
+        if ((c > a) && (c < b) || ((c < a) && (c > b))) {
+            System.out.println("The value c = " + c + " is mid");
+        }
+        if ((c > a) && (c > b)) {
+            System.out.println("The value c = " + c + " is max");
+        }
+
     }
 
-
-    public static void main(String[] args) {
-        MaxMidMin result = new MaxMidMin();
-
-        int a1 = result.aVar(100, 15, 200);
-        int b1 = result.bVar(100, 15, 200);
-        int c1 = result.cVar(100, 15, 200);
-    }
 }
