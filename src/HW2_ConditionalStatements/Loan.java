77d0d55e6d97d88c1f88290842a219ff9f9955d5
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-02-23 21:16:54 +0200

    Loan updated

--- src/HW2_ConditionalStatements/Loan.java
+++ src/HW2_ConditionalStatements/Loan.java
@@ -6,21 +6,23 @@ public class Loan {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the husband`s age: ");
-        int husbandAge = input.nextInt();
         System.out.print("Enter the wife`s age: ");
         int wifeAge = input.nextInt();
-
-        if ((husbandAge > 0) && (husbandAge <= 35) && (wifeAge > 0) && (wifeAge <= 35)) {
-            System.out.println("Mortgage loan is approved by the bank");
-        } else if ((husbandAge > 35) && (wifeAge > 35)) {
+        if (wifeAge>35){
             System.out.println("Mortgage loan is NOT approved by the bank");
-        } else if ((husbandAge > 35) || (wifeAge > 35)) {
-            System.out.println("Mortgage loan is NOT approved by the bank");
-        } else {
-            System.out.println("The data entered is incorrect, please try again");
         }
+        else if ((wifeAge > 0) && (wifeAge <= 35)) {
+            System.out.print("Enter the husband`s age: ");
+            int husbandAge = input.nextInt();
 
+         if  ((husbandAge > 0) && (husbandAge <= 35)) {
+                System.out.println("Mortgage loan is approved by the bank");
+            } else if (husbandAge>35){
+                System.out.println("Mortgage loan is NOT approved by the bank");
+            }
+        }
     }
-
 }
+
+
+
