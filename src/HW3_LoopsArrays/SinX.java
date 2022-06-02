f332eeb01b32a453ca4846544580b68b28a20866
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-06-02 14:00:54 +0300

    sinX updated

--- src/HW3_LoopsArrays/SinX.java
+++ src/HW3_LoopsArrays/SinX.java
@@ -3,16 +3,11 @@ package HW3_LoopsArrays;
 public class SinX {
     public static void main(String[] args) {
 
-        int[] degrees = new int[37];
+       double degrees = 37;
 
-        for (int i = 0; i < degrees.length; i++) {
-            degrees[i] = (i * 10);
+        for (double i = 0; i <=360; i+=10) {
 
-        }
-        for (int i = 0; i < degrees.length; i++) {
-            double rad = degrees[i] * Math.PI / 180;
-            double sin = Math.sin(rad);
-            System.out.println("sin " + degrees[i] + "o" + " = " + sin);
+            System.out.println(Math.sin(Math.toRadians(i)));
         }
     }
 }
