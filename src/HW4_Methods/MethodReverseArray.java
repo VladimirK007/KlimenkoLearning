81c127e86df3e1c92a91f28c57708ed99c8ab0a2
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-06-04 14:05:55 +0300

    1 task added

--- src/HW4_Methods/MethodReverseArray.java
+++ src/HW4_Methods/MethodReverseArray.java
@@ -4,28 +4,23 @@ public class MethodReverseArray {
 
     public static void main(String[] args) {
         int[] Main_Array = {20, 19, 18, 4, 16, 15, 14, 4, 12, 11, 9};
-        int[] Second_Array = {12, 3, 5, 9};
-        int[] Third_Array = {45, 894, 23, 243, 34};
         Reverse.Demo(Main_Array);
-        System.out.println(" ");
-        Reverse.Demo(Second_Array);
-        System.out.println(" ");
-        Reverse.Demo(Third_Array);
 
     }
 }
 
 class Reverse {
-    static void Demo(int A[]) {
-        int[] array1 = new int[A.length];
-        System.out.print("Reversed Array: ");
-        for (int i = A.length - 1; i > 0; ) {
-            for (int j = 0; j < A.length; j++) {
-                array1[j] = A[i];
-                i--;
-                System.out.print(array1[j] + " ");
+    static void Demo(int array[]) {
 
-            }
+        System.out.println("Reverse array:");
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
         }
     }
 }
