cc8b64978ba5eb6b69c39932aa91055c5b233f9e
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-06-09 21:34:43 +0300

    Salary Task added

--- src/HW4_Methods/Salary.java
+++ src/HW4_Methods/Salary.java
@@ -94,12 +94,12 @@ class AgentSalary {
 
     public void TotalSalary() {
         if (workingHours <=160){
-         totalSalary = salary + (workingHours*baseHourRatio) + numberOfSalesBonus + amountOfSalesBonus;
+         totalSalary = workingHours*baseHourRatio + numberOfSalesBonus + amountOfSalesBonus;
         System.out.println("______________________");
         System.out.println(" The Agent`s Salary: " + totalSalary + " USD ");
         }
         if (workingHours > 160) {
-            totalSalary = salary + ((workingHours -(workingHours-160))*baseHourRatio) + numberOfSalesBonus + amountOfSalesBonus;
+            totalSalary = 160*baseHourRatio + ((workingHours-160)*1.5) + numberOfSalesBonus + amountOfSalesBonus;
             System.out.println("______________________");
             System.out.println(" The Agent`s Salary: " + totalSalary + " USD ");
 
