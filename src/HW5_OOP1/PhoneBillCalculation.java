7585163d564663ffd60bfa85af7af4cd5b0a711e
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-06-15 21:31:03 +0300

    PhoneBillUpdated

--- src/HW5_OOP1/PhoneBillCalculation.java
+++ src/HW5_OOP1/PhoneBillCalculation.java
@@ -2,27 +2,24 @@ package HW5_OOP1;
 
 public class PhoneBillCalculation {
     public static void main(String[] Args) {
-        PhoneBillInput client1 = new PhoneBillInput(200,9.5,
-                550,58,28,30);
-        client1.getPhoneBill();
+        PhoneBillInput client1 = new PhoneBillInput(200, 9.5,
+                550, 58, 28, 30);
+        System.out.println(client1.getBill());
 
     }
 }
 
 class PhoneBillInput {
 
-    double baseRate;
-    double usedTrafficGB;
-    double inetPrice;
-    double callDuration;
-    double callPrice;
-    double smsNumber;
-    double smsPrice;
-    double callsInZone1;
-    double callsInZone2;
-    double foreign1CallPrice;
-    double foreign2CallPrice;
-    double totalPhonePrice;
+    private double baseRate;
+    private double usedTrafficGB;
+    private double callDuration;
+    private double callPrice;
+    private double smsNumber;
+    private double smsPrice;
+    private double callsInZone1;
+    private double callsInZone2;
+    private double totalPhonePrice;
 
     public PhoneBillInput(double baseRate, double usedTrafficGB,
                           double callDuration, double smsNumber,
@@ -33,10 +30,15 @@ class PhoneBillInput {
         this.smsNumber = smsNumber;
         this.callsInZone1 = callsInZone1;
         this.callsInZone2 = callsInZone2;
+        setBill(baseRate, usedTrafficGB,
+                callDuration, smsNumber,
+                callsInZone1, callsInZone2);
     }
 
-    public double getPhoneBill() {
 
+    public double internetPrice(double usedTrafficGB, double baseRate) {
+
+        double inetPrice;
         if (usedTrafficGB >= 8) {
             inetPrice = baseRate + (usedTrafficGB - 8) * 100;
         } else {
@@ -44,7 +46,10 @@ class PhoneBillInput {
         }
         System.out.println("______________________");
         System.out.println("Internet price = " + inetPrice + " UAH");
+        return inetPrice;
+    }
 
+    public double callPrice(double callDuration) {
 
         if (callDuration <= 500) {
             callPrice = callDuration * 0.5;
@@ -53,7 +58,10 @@ class PhoneBillInput {
             callPrice = 500 * 0.5 + ((callDuration - 500) * 0.75);
         }
         System.out.println("Call price: " + callPrice + " UAH");
+        return callPrice;
+    }
 
+    public double smsPrice(double smsNumber) {
 
         if (smsNumber > 50) {
             smsPrice = (smsNumber - 50) * 1.5 + 50;
@@ -63,19 +71,39 @@ class PhoneBillInput {
         }
 
         System.out.println("SMS price: " + smsPrice + " UAH");
+        return smsPrice;
+    }
 
-
-        foreign1CallPrice = 1.5 * callsInZone1;
+    public double foreignCallPrice1(double callsInZone1) {
+        double foreign1CallPrice = 1.5 * callsInZone1;
         System.out.println("Foreign calls in 1 zone: " + foreign1CallPrice + " UAH");
+        return foreign1CallPrice;
+    }
 
-
-        foreign2CallPrice = 2 * callsInZone2;
+    double foreignCallPrice2(double callsInZone2) {
+        double foreign2CallPrice = 2 * callsInZone2;
         System.out.println("Foreign calls in 2 zone: " + foreign2CallPrice + " UAH");
+        return foreign2CallPrice;
+    }
+
+    public void setBill(double baseRate, double usedTrafficGB,
+                        double callDuration, double smsNumber,
+                        double callsInZone1, double callsInZone2) {
+        this.baseRate = baseRate;
+        this.usedTrafficGB = usedTrafficGB;
+        this.callDuration = callDuration;
+        this.smsNumber = smsNumber;
+        this.callsInZone1 = callsInZone1;
+        this.callsInZone2 = callsInZone2;
+        totalPhonePrice = (internetPrice(usedTrafficGB, baseRate) +
+                callPrice(callDuration) + smsPrice(smsNumber) +
+                foreignCallPrice1(callsInZone1) + foreignCallPrice2(callsInZone2)) * 1.07;
+
+    }
 
 
-        totalPhonePrice = (inetPrice + callPrice + smsPrice + foreign1CallPrice + foreign2CallPrice) * 1.07;
-        System.out.println("______________________ ");
-        System.out.println("Total Phone Bill: " + totalPhonePrice + " UAH");
+    double getBill() {
         return totalPhonePrice;
     }
+
 }
